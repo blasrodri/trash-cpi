3f370beef880a406ed14749a65f9ba4c5fbdfa0c
Blas Rodriguez Irizar <rodrigblas@gmail.com>
2022-04-21 16:06:50 +0200

    repo

---
+++ programs/asd-puppet/src/lib.rs
@@ -0,0 +1,26 @@
+use anchor_lang::prelude::*;
+
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+#[program]
+pub mod asd_puppet {
+    use super::*;
+
+    pub fn set_data(ctx: Context<SetData>, num: u64) -> Result<()> {
+        // here we shuold also check for authority...
+        ctx.accounts.data_acc.num = num;
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct SetData<'info> {
+    pub data_acc: Account<'info, Data>,
+    /// CHECK: blabl
+    pub authority: UncheckedAccount<'info>,
+}
+
+#[account]
+pub struct Data {
+    pub num: u64,
+}
---
+++ programs/asd/src/lib.rs
@@ -0,0 +1,58 @@
+use anchor_lang::prelude::*;
+use asd_puppet::{program::AsdPuppet, Data};
+
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+#[program]
+pub mod asd {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        // craete the PDA
+        Ok(())
+    }
+
+    pub fn set_data_cpi(ctx: Context<SetDataCPI>, bump: u8, data: u64) -> Result<()> {
+        // given the PDA, do the CPI with the signer
+        let seeds = &[&[b"data", bytemuck::bytes_of(&bump)][..]];
+        let asd_puppet_id = ctx.accounts.asd_puppet.to_account_info();
+        let callee_accounts = asd_puppet::cpi::accounts::SetData {
+            data_acc: ctx.accounts.data_acc.to_account_info(),
+            authority: ctx.accounts.callee_authority.to_account_info(),
+        };
+        let cpi_ctx = CpiContext::new_with_signer(asd_puppet_id, callee_accounts, seeds);
+        asd_puppet::cpi::set_data(cpi_ctx, data)
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(
+        init,  
+        seeds = [b"data".as_ref()],
+        bump,
+        space = 8 + 8,
+        payer = signer
+    )]
+    pub stored_data: Account<'info, Data>,
+    
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(bump: u8, _data: u64)]
+pub struct SetDataCPI<'info> {
+
+    #[account(
+        mut,
+        seeds = [b"data".as_ref()],
+        bump,
+    )]
+    pub data_acc: Account<'info, Data>,
+    /// CHECK: fodakjfldsf
+    pub callee_authority: UncheckedAccount<'info>,
+    pub asd_puppet: Program<'info, AsdPuppet>,
+}
